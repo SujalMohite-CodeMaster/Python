b4eadd7acca6d289f96c2d81cc3f66da598fee4e
SujalMohite-CodeMaster <mohitesujal35@gmail.com>
2024-12-28 21:12:39 +0530

    general inbuilt functions in python code added

---
+++ function.py
@@ -0,0 +1,27 @@
+x = -7.8
+num = 3
+print(x)
+print(type(x))
+
+#abs() function return the positive absolute value 
+print(abs(x))
+print(type(x))
+
+# using math library for importing the exp() function
+import math
+print(math.exp(num))
+
+print(math.exp)
+
+print(math.pi)
+
+print(math.sqrt(16))
+
+# some general built in functions in python
+# max() is return the max value 
+res = max(1,2,3,4,23,14,53,53,54)
+print(res)
+
+#min() is return the minimum value 
+res = min(2,23,43,74,83,4)
+print(res)
